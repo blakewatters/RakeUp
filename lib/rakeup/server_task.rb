30d621aec3aa4214b37e2da3a66a466190245aa9
Blake Watters <blake@layer.com>
2014-06-09 21:04:37 -0700

    Change default port to localhost instead of `nil`

--- lib/rakeup/server_task.rb
+++ lib/rakeup/server_task.rb
@@ -24,7 +24,7 @@ module RakeUp
   
     def initialize(name = :server)
       @name = name
-      @host = nil
+      @host = 'localhost'
       @port = 4567
       @pid_file = "tmp/#{name}.pid"
       @rackup_file = "#{name}.ru"
